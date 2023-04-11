7d71a4f845f4c2875adf64220fbb4c68ffb90717
Sylvie Canongia <sylvie.canongia.dev@gmail.com>
2023-04-11 20:49:43 +0200

    style: added a style module for home page(index) in the styles folder

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,8 @@
 import Head from 'next/head';
 import { NextPageWithLayout } from './_app';
-
-// import Image from 'next/image';
-// import { Inter } from 'next/font/google'
-// import styles from '@/styles/Home.module.css'
-
-// const inter = Inter({ subsets: ['latin'] })
+import CatCard from '@/domain/components/cards/cat/CatCard';
+import { mockCatCardProps } from '@/domain/components/cards/cat/CatCard.mock';
+import styles from './../styles/home.module.scss';
 
 const Home: NextPageWithLayout = () => {
   return (
@@ -14,6 +11,16 @@ const Home: NextPageWithLayout = () => {
         <title>Chadamour</title>
       </Head>
       <h1>Bienvenue sur Chadamour, un site pour les amoureux des chats !</h1>
+      <div className={styles.container}>
+        <CatCard {...mockCatCardProps.base} />
+        <CatCard {...mockCatCardProps.base} />
+        <CatCard {...mockCatCardProps.base} />
+        <CatCard {...mockCatCardProps.base} />
+        <CatCard {...mockCatCardProps.base} />
+        <CatCard {...mockCatCardProps.base} />
+        <CatCard {...mockCatCardProps.base} />
+        <CatCard {...mockCatCardProps.base} />
+      </div>
     </>
   );
 };
